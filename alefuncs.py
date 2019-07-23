17b5228e4bafed55188d0892f721faff625607d8
alec-djinn <alec_djinn@yahoo.com>
2019-07-23 09:02:39 +0200

    new func: compare_dict

--- alefuncs.py
+++ alefuncs.py
@@ -52,6 +52,16 @@ ale_palette = {'purple':"#9b59b6",
                'green':"#2ecc71"}
 
 
+def compare_dict(d1, d2):
+    d1_keys = set(d1.keys())
+    d2_keys = set(d2.keys())
+    intersect_keys = d1_keys.intersection(d2_keys)
+    added = d1_keys - d2_keys
+    removed = d2_keys - d1_keys
+    modified = {o : (d1[o], d2[o]) for o in intersect_keys if d1[o] != d2[o]}
+    same = set(o for o in intersect_keys if d1[o] == d2[o])
+    return added, removed, modified, same
+
 
 class DotNotDict:
     '''
